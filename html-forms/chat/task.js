3e64bf90daafb93c10a1cc2a9463d3ee9754e802
Юлия Смирнова <dolphin_99@mail.ru>
2022-08-23 22:25:48 +0300

    Homework5 again

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -1,30 +1,29 @@
 let chatWidget = document.querySelector('.chat-widget');
+let chatInput = document.getElementById('chat-widget__input');
+let date = new Date();
+let botMessages = [
+  'Сейчас узнаю!',
+  'Подождите секунду!',
+  'Стараюсь Вам помочь',
+  'Это очень интересно!',
+];
+let botQuestions = [
+  'Почему молчите?',
+  'У Вас больше нет вопросов?',
+  'Могу я ещё чем-то помочь?'
+];
+let randomBotMessage = botMessages[Math.floor(Math.random() * botMessages.length)];
+let randomBotQuestion = botQuestions[Math.floor(Math.random() * botQuestions.length)];
+const messages = document.querySelector('.chat-widget__messages');
+let chatArea = document.querySelector('.chat-widget__messages-container');
+let currentTime = `${date.getHours()} : ${date.getMinutes()}`;
 
 chatWidget.addEventListener('click', () => {
   chatWidget.classList.add('chat-widget_active');
 });
 
-document.addEventListener('keyup', event => {
-  let chatInput = document.getElementById('chat-widget__input');
-  let date = new Date();
-  let botMessages = [
-    'Сейчас узнаю!',
-    'Подождите секунду!',
-    'Стараюсь Вам помочь',
-    'Это очень интересно!',
-  ];
-  let botQuestions = [
-    'Почему молчите?',
-    'У Вас больше нет вопросов?',
-    'Могу я ещё чем-то помочь?'
-  ];
-  let randomBotMessage = botMessages[Math.floor(Math.random() * botMessages.length)];
-  let randomBotQuestion = botQuestions[Math.floor(Math.random() * botQuestions.length)];
-  let chatArea = document.querySelector('.chat-widget__messages-container');
-  let currentTime = `${date.getHours()} : ${date.getMinutes()}`;
-  const messages = document.querySelector('.chat-widget__messages');
-
-  let timer = setTimeout(() => {
+let timer = () => {
+  setTimeout(() => {
     messages.innerHTML += `
       <div class="message">
         <div class="message__time">
@@ -32,15 +31,20 @@ document.addEventListener('keyup', event => {
         <div class="message__text">${randomBotQuestion}</div>
       </div>
       `;
-    }, 10000);
+      chatArea.scrollTop = chatArea.scrollHeight;
+  }, 5000);
+};
 
-  chatInput.addEventListener('focus', () => {
-    timer();
-  });
+chatInput.addEventListener('focus', () => {
+  timer();
+});
 
-  if ((event.code === 'Enter') && (chatInput.value != '')) {
-    clearTimeout(timer);
-        
+document.addEventListener('keyup', event => {
+  let chatArea = document.querySelector('.chat-widget__messages-container');
+  let currentTime = `${date.getHours()} : ${date.getMinutes()}`;
+  clearTimeout(timer);
+
+  if ((event.code === 'Enter') && (chatInput.value != '')) {        
     messages.innerHTML += `
       <div class="message message_client">
         <div class="message__time">
@@ -61,5 +65,6 @@ document.addEventListener('keyup', event => {
     `;
     
     chatArea.scrollTop = chatArea.scrollHeight;
+    timer();
   };
 });
